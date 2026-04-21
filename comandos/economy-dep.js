a5e887ca403e2f9222d9ccfdffc5fe50dd268f8b
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 09:39:01 -0400

    Actualizar economy-dep.js

--- comandos/economy-dep.js
+++ comandos/economy-dep.js
@@ -8,6 +8,11 @@ const depCommand = {
     name: 'deposit',
     alias: ['dep', 'd', 'depositar'],
     category: 'economy',
+    isOwner: false,
+    noPrefix: true,
+    isAdmin: false,
+    isGroup: false,
+
     run: async (conn, m, args) => {
         try {
             const user = m.sender.split('@')[0];
