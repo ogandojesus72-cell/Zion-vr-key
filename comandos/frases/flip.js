0c1e20a96dca23fc19bfe9f5036f4989519c9aa4
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 12:39:50 -0400

    Crear flip.js

---
+++ comandos/frases/flip.js
@@ -0,0 +1,14 @@
+export const flipFrases = {
+    win: [
+        "¡Increíble! La moneda cayó a tu favor. Ganaste ¥1,000.",
+        "¡Suerte de principiante! Has ganado ¥1,000 esta vez.",
+        "¡La moneda te ama! Sumas ¥1,000 a tu cartera.",
+        "¡Boom! Ganaste. ¥1,000 directos a tu bolsillo."
+    ],
+    lose: [
+        "¡Qué mala suerte! La moneda te dio la espalda. Perdiste ¥1,000.",
+        "Esta vez la casa gana. Perdiste ¥1,000.",
+        "¡Casi! Pero no. La moneda cayó del otro lado. Perdiste ¥1,000.",
+        "¡F! Perdiste ¥1,000. ¿Vas a dejar las cosas así?"
+    ]
+};
