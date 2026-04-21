d2b5a1c6c8ead8c5bf6ed7f55af75c844d7ba353
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-21 09:28:04 -0400

    Actualizar main-ping.js

--- comandos/main-ping.js
+++ comandos/main-ping.js
@@ -1,37 +1,28 @@
 import { config } from '../config.js';
-import { contextConfig } from '../config/visuals_context.js';
 
 const pingCommand = {
     name: 'ping',
-    alias: ['p', 'speed'],
+    alias: ['p', 'speed', 'latencia'],
     category: 'main',
+    isOwner: false,
+    noPrefix: true,
+    isAdmin: false,
+    isGroup: false,
+
     run: async (conn, m) => {
         try {
             const start = Date.now();
-            const { key } = await m.reply(`*${config.visuals.emoji2}* \`Calculando...\``);
-            const latencia = Date.now() - start;
+            const end = Date.now();
+            const latencia = end - start;
 
-            const texto = `*${config.visuals.emoji3}* \`KAZUMA PING\` *${config.visuals.emoji3}*\n\n*${config.visuals.emoji4} Latencia:* ${latencia} ms\n*${config.visuals.emoji} Estado:* Operativo`;
+            const textoPing = `*${config.visuals.emoji3}* \`KAZUMA PING\` *${config.visuals.emoji3}*\n\n*${config.visuals.emoji4} Velocidad:* ${latencia} ms\n*${config.visuals.emoji} Estado:* Online\n\n> *${config.visuals.emoji2}* \`SISTEMA OPERATIVO\``;
 
-            await conn.sendMessage(m.chat, {
-                text: texto,
-                contextInfo: {
-                    externalAdReply: {
-                        title: contextConfig.title,
-                        body: contextConfig.body,
-                        thumbnailUrl: contextConfig.image,
-                        sourceUrl: contextConfig.canal,
-                        mediaType: 1,
-                        showAdAttribution: false,
-                        renderLargerThumbnail: true // Esto fuerza a que se vea mejor
-                    }
-                }
+            await conn.sendMessage(m.chat, { 
+                text: textoPing 
             }, { quoted: m });
 
-            await conn.sendMessage(m.chat, { delete: key });
-
         } catch (err) {
-            m.reply(`🚀 Latencia: ${Date.now() - start} ms`);
+            console.error(err);
         }
     }
 };
