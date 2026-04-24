3ef5f10e7c261157336cb43d1ad6212b030e5002
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-24 11:19:22 -0400

    Actualizar admins-close.js

--- comandos/admins-close.js
+++ comandos/admins-close.js
@@ -3,7 +3,7 @@ import { avisos } from '../config/avisos.js';
 
 const closeGroup = {
     name: 'close',
-    alias: ['cerrargroup', 'cerrardatos', 'cerrar'],
+    alias: ['cerrargroup', 'cerrar'],
     category: 'admins',
     isAdmin: true,
     noPrefix: true,
@@ -19,12 +19,12 @@ const closeGroup = {
             }
 
             if (groupMetadata.announce) {
-                return m.reply(`*${config.visuals.emoji2}* El grupo ya se encuentra en modo restrictivo (cerrado).\n\n> ¡El silencio ya impera en este sector!`);
+                return m.reply(`*${config.visuals.emoji2}* El grupo ya se encuentra cerrado.\n\n> ¡El silencio ya impera en este sector!`);
             }
 
             await conn.groupSettingUpdate(m.chat, 'announcement');
             
-            m.reply(`*${config.visuals.emoji3} \`GRUPO CERRADO\` ${config.visuals.emoji3}*\n\nSe ha activado el modo restrictivo. Solo los administradores pueden enviar mensajes.\n\n> ¡Momento de silencio en el servidor!`);
+            m.reply(`*${config.visuals.emoji3} \`GRUPO CERRADO\` ${config.visuals.emoji3}*\n\nSe ha activado el modo restrictivo.\n\n> ¡Momento de silencio en el servidor!`);
         } catch (e) {
             m.reply(`*${config.visuals.emoji2}* Error al intentar cerrar el grupo.`);
         }
