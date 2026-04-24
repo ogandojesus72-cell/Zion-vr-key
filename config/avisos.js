10c7d5c950f4fbfd9e3d3491d2f8ae07b674599d
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-24 11:21:17 -0400

    Eliminar avisos.js

--- config/avisos.js
+++
@@ -1,5 +0,0 @@
-import { config } from './config.js';
-
-export const avisos = {
-    noBotAdmin: `*${config.visuals.emoji2}* El bot no posee rango de Administrador. No tengo poder para alterar los ajustes del grupo.\n\n> ¡Solicita el rango si deseas automatizar esta función!`
-};
