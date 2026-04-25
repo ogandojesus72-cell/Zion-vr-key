b1c29251976bfcd6ce83a26f7bf039a3ee3e49b1
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-25 14:00:07 -0400

    Actualizar main-ping.js

--- comandos/main-ping.js
+++ comandos/main-ping.js
@@ -1,4 +1,6 @@
 import { config } from '../config.js';
+import fs from 'fs-extra';
+import path from 'path';
 
 const pingCommand = {
     name: 'ping',
@@ -20,8 +22,20 @@ const pingCommand = {
             const end = Date.now();
             const latencia = end - start;
 
+            const botNumber = conn.user.id.split(':')[0];
+            const settingsPath = path.resolve(`./sesiones_subbots/${botNumber}/settings.json`);
+            
+            let displayShortName = config.botName;
+
+            if (fs.existsSync(settingsPath)) {
+                const localData = await fs.readJson(settingsPath);
+                if (localData.shortName) {
+                    displayShortName = localData.shortName;
+                }
+            }
+
             await conn.sendMessage(m.chat, { 
-                text: `*${config.visuals.emoji3}* \`KAZUMA PING\` *${config.visuals.emoji3}*\n\n*${config.visuals.emoji4} Velocidad:* ${latencia} ms\n*${config.visuals.emoji} Estado:* Online\n\n> *${config.visuals.emoji2}* \`SISTEMA OPERATIVO\``,
+                text: `*${config.visuals.emoji3}* \`${displayShortName.toUpperCase()} PING\` *${config.visuals.emoji3}*\n\n*${config.visuals.emoji4} Velocidad:* ${latencia} ms\n*${config.visuals.emoji} Estado:* Online\n\n> *${config.visuals.emoji2}* \`SISTEMA OPERATIVO\``,
                 edit: pingMsg.key 
             });
 
