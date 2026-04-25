c8e4ed4f34534b124478153a1228771e354d36db
Félix Ofx <xlfelixgamer@gmail.com>
2026-04-25 08:53:44 -0400

    Actualizar config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 export const configPriority = {
     stickers: {
-        packname: '✿︎   𝐊𝐚𝐳𝐮𝐦𝐚 𝐁𝐨𝐭   ✿︎\n➪ https://kazuma.giize.com\n\n❁ commands »\nhttps://kazuma.giize.com/commands',
+        packname: '✿︎   𝐊𝐚𝐳𝐮𝐦𝐚 𝐁𝐨𝐭   ✿︎\n➪ https://kazuma.giize.com\n\n  ❁ commands »\nhttps://kazuma.giize.com/commands',
         packauthor: '✿︎ Bot »\n✰ Kazuma\n \n      ❁ Usuario »\n   ✰ @(userName)'
     },
     system: {
